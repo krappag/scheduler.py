09821f431d87b1bd01855ca2c2f820effdb67b32
krappag <53987384+krappag@users.noreply.github.com>
2019-08-11 01:14:09 -0700

    Update Scheduler.py

--- Scheduler.py
+++ Scheduler.py
@@ -3,32 +3,27 @@ class Doctor:
     def __init__(self, name):
         self.name = name
 
-        self.number_of_patient = []
+        self.list_of_patient = []
         self.availability = []
 
     def __str__(self):
         return "Hi, this is Dr. {a}".format(a=self.name)
 
     def add_patient(self, patient):
-        for dates in self.availability:
-            if len(self.number_of_patient) < 4:
-                self.number_of_patient.append(patient)
-                pring(dates)
-            else:
-                print("Dr. {a} has no more appointment for today".format(a=self.name))
+        self.list_of_patient.
 
     def remove_patient(self, patient):
         for dates in self.availability:
-            if len(self.number_of_patient) == 0 and self.availability[dates]:
+            if len(self.list_of_patient) == 0 and self.availability[dates]:
                 print("There is no patient left")
             else:
-                self.number_of_patient.remove(patient.name)
+                self.list_of_patient.remove(patient.name)
 
     def add_days(self, *dates):
         self.availability.append(dates)
 
     def remove_days(self, *date):
-        for num in range(len(self.availability[1])):
+        for num in range(len(self.availability)):
             if date == self.availability:
                 self.availability.remove(date)
 
@@ -36,13 +31,11 @@ class Doctor:
         print("Dr. {a} patient's list for {b}:\n".format(a=self.name, b=date))
 
 
-
 class Patient:
 
     def __init__(self, name, sickness, date_of_appointment):
         self.name = name
         self.sickness = sickness
-        self.date = date_of_appointment
 
     def __str__(self):
         return "Name: {a}\n Sickness: {b}\n\n".format(a=self.name, b=self.sickness)
@@ -78,5 +71,4 @@ phil = Patient('Phil', 'Flu', '10 August')
 andrew = Patient('Andrew', 'Flu', '11 August')
 jace = Patient('Jace', 'too fat', '10 August')
 anthony.add_days('7 August', '8 August', '9 August', '10 August')
-print(dates for dates in anthony.availability)
 anthony.print_list_of_patient('7 August')
